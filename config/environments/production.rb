aca578f2fbc3302e81156de9bb713741353bf3de
hotanlamvnn <hotanlamvnn@gmail.com>
2015-06-10 17:22:34 +0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,15 +90,15 @@ Rails.application.configure do
   # メールに記載する本サービスのURL
   config.action_mailer.default_url_options = { :host => 'www.example.com' }
 
-  # SMTPの指定
+  # # SMTPの指定
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => 'smtp.sendgrid.net',
-    :port => '587',
-    :domain => 'heroku.com',
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  # config.action_mailer.smtp_settings = {
+  #   :address => 'smtp.sendgrid.net',
+  #   :port => '587',
+  #   :domain => 'heroku.com',
+  #   :user_name => ENV['SENDGRID_USERNAME'],
+  #   :password => ENV['SENDGRID_PASSWORD'],
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 end
